2c148c86e62fe4fb62a17c0f2aa794756d1a7859
myu-suke <mo.y.0930@gmail.com>
2022-07-04 23:57:04 +0900

    axios config

--- api/fetcher.ts
+++ api/fetcher.ts
@@ -8,10 +8,34 @@ const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    HOGE: "hogehoge"
+    HOGE: "hogehoge" // 任意のヘッダー
   }
 });
-// TODO リトライはReactQueryで実施可能、intercepterはここに定義
 
+// FIXME
+// リクエスト時の共通処理
+axiosInstance.interceptors.request.use((request) => {
+  if (!request) return request;
+  // 認証トークンなど（ストアなどから取得するメソッドをコール）
+  if (request.headers) {
+    request.headers.Authorization = `Bearer <tokennnnnnnn>`;
+  }
+  // TODO 削除
+  window.console.info(request);
+
+  return request;
+});
+
+// レスポンス時の共通処理
+axiosInstance.interceptors.response.use(
+  (response) => response, // 成功時
+  (error) => {
+    // 失敗時
+    // セッション・トークンのリフレッシュ処理等
+    window.console.error(JSON.stringify(error, null, 2));
+  }
+);
+
+// aspida
 const fetcher = aspida(axiosInstance);
 export { fetcher };
